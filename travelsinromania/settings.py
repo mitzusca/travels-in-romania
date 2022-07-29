7138f1a3fd852c571ac6b79c7f3b9de6c3298a51
mitzusca <mihai_93_hd@yahoo.com>
2022-07-29 23:01:44 +0000

    Building the admin site

--- travelsinromania/settings.py
+++ travelsinromania/settings.py
@@ -45,9 +45,10 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-
+    'django_summernote',
     'blog',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
